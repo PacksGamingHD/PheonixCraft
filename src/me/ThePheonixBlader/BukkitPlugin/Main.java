9cd39f5f4138e55f5f8c9c160bef3b4aa4cf16ca
PacksGamingHD <craftyswans@gmail.com>
2014-09-28 21:07:16 -0700

    Plugin revamp

--- src/me/ThePheonixBlader/BukkitPlugin/Main.java
+++ src/me/ThePheonixBlader/BukkitPlugin/Main.java
@@ -13,25 +13,35 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-	
-	public boolean onCommand(Command sender, Command command, String label, String[] args) {
-			if(label.equalsIgnoreCase("test")){
-				if(sender instanceof Player){
-					Player player = (Player) sender;
-					player.getInventory().addItem(new ItemStack(Material.DIAMOND));
-					player.sendMessage("" + ChatColor.AQUA + "Here have A...");
-					player.sendMessage("§b§lDIAMOND!!!");
-				} else {
-					sender.setUsage("You dont have permission to run this command");
-				}
-			}
-			return true;
-	}
-	Logger logger = Logger.getLogger("Minecraft");
-	
-	public void onEnable(){
-		
-		logger.info("Thanks for watching guys! A like would be much appreciated. I'll see you guys... NEXT TIME");
-	}
+    @Override
+    public void onEnable() {
+        getLogger().info("Plugin Enabled!");
+        for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+        playerList.put(player.getName(), playerData(player));
+    }
 
+ 
+    @Override
+    public void onDisable() {
+        getLogger().info("Plugin Disabled!");
+    }
 }
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	if (cmd.getName().equalsIgnoreCase("test")) { 
+		if(sender instanceof Player){
+		Player player = (Player) sender;
+		player.getInventory().addItem(new ItemStack(Material.DIAMOND));
+		player.sendMessage("" + ChatColor.AQUA + "Here have A...");
+		player.sendMessage("Â§bÂ§lDIAMOND!!!");
+		} else {
+		sender.setUsage("You dont have permission to run this command");
+		}
+		return true;
+	} 
+	return false; 
+   
+   }
+}   
+
